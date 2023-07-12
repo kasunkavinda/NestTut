94878de6855b5f98b2fab48edf28b190e3b66ebe
kasunkavinda <kasunkavinda.bc@gmail.com>
2023-07-12 22:20:10 +0530

    Controller implementations in "ninjas"

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,25 +1,43 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('type') type: string) {
+    return type;
   }
 
   @Get(':id')
-  getOneNinja() {
-    return {};
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
 
   @Post()
-  createNinja() {
-    return {};
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      data: createNinjaDto,
+    };
   }
 
   @Put(':id')
-  updateNinja() {
-    return {};
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id: id,
+      name: updateNinjaDto.name,
+    };
   }
 
   @Delete(':id')
